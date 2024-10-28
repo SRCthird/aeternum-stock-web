091b4eb8ab65f21ecb3afb6a44ecdefb8fab67c5
Stephen Chryn <SRCthird@gmail.com>
2024-10-28 15:54:31 -0400

    Add scrap and release actions

--- inventory/views.py
+++ inventory/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from django.urls import reverse
 from django.contrib import messages
 
-from .models import InventoryBay, InventoryBayLot
+from .models import InventoryBay, InventoryBayLot, ProductLot
 from .forms import ProductLotForm, InventoryTransferForm
 
 
@@ -41,22 +41,43 @@ def create_item(request):
 
 @login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
 def transfer(request):
+    product_lot_id = request.GET.get('product_lot')
+    from_inventory_bay_id = request.GET.get('from_inventory_bay')
+    action = request.GET.get('action')
+    to_inventory_bay_name = None
+    if action == 'release':
+        to_inventory_bay_name = 'Released'
+    elif action == 'scrapped':
+        to_inventory_bay_name = 'Scrapped'
+    quantity = request.GET.get('quantity')
+
+    initial_data = {}
+    if product_lot_id:
+        initial_data['product_lot'] = get_object_or_404(
+            ProductLot, id=product_lot_id)
+    if from_inventory_bay_id:
+        initial_data['from_inventory_bay'] = get_object_or_404(
+            InventoryBay, id=from_inventory_bay_id)
+    if to_inventory_bay_name:
+        initial_data['to_inventory_bay'] = get_object_or_404(
+            InventoryBay, name=to_inventory_bay_name)
+    if quantity:
+        initial_data['quantity'] = quantity
+
     if request.method == 'POST':
-        form = InventoryTransferForm(request.POST)
+        form = InventoryTransferForm(request.POST, initial=initial_data)
         if form.is_valid():
             try:
                 form.save()
-                messages.success(
-                    request, "Inventory transfer completed successfully.")
-                return redirect(reverse('index'))
+                return redirect('index')
             except ValueError as e:
                 form.add_error(None, str(e))
     else:
-        form = InventoryTransferForm()
+        form = InventoryTransferForm(initial=initial_data)
 
     return render(request, 'home/transfer.html', {
-        'title': 'Inventory Transfer',
-        'form': form
+        'form': form,
+        'title': 'Inventory Transfer'
     })
 
 
