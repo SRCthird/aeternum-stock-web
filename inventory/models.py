1344f5f697af62dbaa5db2325348a65964c76985
Stephen Chryn <SRCthird@gmail.com>
2024-10-28 14:01:47 -0400

    Fix up registration views

--- inventory/models.py
+++ inventory/models.py
@@ -89,21 +89,17 @@ class InventoryBay(models.Model):
             lot_quantity = self.lot_quantities.get(product_lot=product_lot)
             return lot_quantity.quantity
         except InventoryBayLot.DoesNotExist:
-            # If the lot doesn't exist in this bay, return 0 quantity
             return 0
 
     def adjust_quantity(self, product_lot, quantity_change):
         """Adjust the quantity of a specific ProductLot in this InventoryBay."""
-        # Try to get the InventoryBayLot object, or create it if it doesn't exist
         lot_quantity, created = InventoryBayLot.objects.get_or_create(
             inventory_bay=self,
             product_lot=product_lot
         )
 
-        # Update the quantity
         lot_quantity.quantity += quantity_change
 
-        # Ensure the quantity doesn't go negative
         if lot_quantity.quantity < 0:
             raise ValueError(f"Quantity for {product_lot} in {
                              self.name} cannot be negative.")
@@ -145,7 +141,7 @@ class InventoryTransfer(models.Model):
         on_delete=models.RESTRICT,
         related_name="outgoing_transfers",
         null=True,
-        blank=True  # Allows for transfers into the system
+        blank=True
     )
     to_inventory_bay = models.ForeignKey(
         InventoryBay,
@@ -158,7 +154,6 @@ class InventoryTransfer(models.Model):
     history = HistoricalRecords(user_model=settings.AUTH_USER_MODEL)
 
     def save(self, *args, **kwargs):
-        # Check for sufficient quantity in `from_inventory_bay` before transfer
         if self.from_inventory_bay:
             current_quantity = self.from_inventory_bay.get_lot_quantity(
                 self.product_lot)
@@ -168,13 +163,10 @@ class InventoryTransfer(models.Model):
                         self.from_inventory_bay} for transfer of {self.product_lot}."
                 )
 
-        # Check if adding this lot would exceed max_unique_lots in `to_inventory_bay`
         existing_lots_with_quantity = self.to_inventory_bay.lot_quantities.filter(
             quantity__gt=0)
 
-        # Count unique lots in `to_inventory_bay` that currently have a quantity greater than zero
-        if existing_lots_with_quantity.count() >= self.to_inventory_bay.max_unique_lots:
-            # If the `product_lot` is not already present in the inventory bay
+        if existing_lots_with_quantity.count() >= self.to_inventory_bay.max_unique_lots and self.to_inventory_bay.max_unique_lots != -1:
             if not existing_lots_with_quantity.filter(product_lot=self.product_lot).exists():
                 raise ValueError(
                     f"Cannot transfer {self.product_lot} to {
@@ -185,7 +177,6 @@ class InventoryTransfer(models.Model):
 
         super().save(*args, **kwargs)
 
-        # Adjust quantities in both source and destination InventoryBays
         if self.from_inventory_bay:
             self.from_inventory_bay.adjust_quantity(
                 self.product_lot, -self.quantity)
--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
 from django.urls import reverse
 from django.contrib import messages
-from .forms import InventoryTransferForm
 
-from .forms import ProductLotForm
+from .models import InventoryBay, InventoryBayLot
+from .forms import ProductLotForm, InventoryTransferForm
 
 
 @login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
@@ -18,16 +19,27 @@ def create_item(request):
     if request.method == "POST":
         form = ProductLotForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(reverse('transfer'))
+            new_product_lot = form.save()
+
+            starting_bay = get_object_or_404(InventoryBay, name="Operations")
+
+            InventoryBayLot.objects.create(
+                inventory_bay=starting_bay,
+                product_lot=new_product_lot,
+                quantity=new_product_lot.quantity
+            )
+
+            return redirect(reverse('index'))
     else:
         form = ProductLotForm()
+
     return render(request, 'home/create_item.html', {
         'title': 'Create Product Lot',
         'form': form
     })
 
 
+@login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
 def transfer(request):
     if request.method == 'POST':
         form = InventoryTransferForm(request.POST)
@@ -49,13 +61,22 @@ def transfer(request):
 
 
 @login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
-def inventory_transfer(request):
-    action = request.GET.get('action', 'transfer')
-    context = {'title': 'Inventory Transfer' if action ==
-               'transfer' else 'Scrap or Release Item', 'action': action}
-    return render(request, 'home/transfer.html', context)
-
-
-@login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
 def find_lot(request):
-    return "not implimented"
+    # Get the search query from the URL parameters
+    query = request.GET.get('q', '')
+    inventory_bay_lots = InventoryBayLot.objects.filter(
+        quantity__gt=0).select_related('inventory_bay__warehouse_name', 'product_lot')
+
+    if query:
+        # Filter by Product Lot's lot number, Inventory Bay's name, or Warehouse's name
+        inventory_bay_lots = inventory_bay_lots.filter(
+            Q(product_lot__lot_number__icontains=query) |
+            Q(inventory_bay__friendly_name__icontains=query) |
+            Q(inventory_bay__warehouse_name__name__icontains=query)
+        )
+
+    return render(request, 'home/inventory_bay_lots_list.html', {
+        'title': 'Inventory Bay Lots',
+        'inventory_bay_lots': inventory_bay_lots,
+        'search_query': query
+    })
