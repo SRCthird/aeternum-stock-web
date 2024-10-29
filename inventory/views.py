99c94c8d4f90a5229d2b8875a8b948ef42b04944
Stephen Chryn <SRCthird@gmail.com>
2024-10-29 13:16:11 -0400

    Change starting bay name

--- inventory/views.py
+++ inventory/views.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Q
 from django.urls import reverse
-from django.contrib import messages
 
 from .models import InventoryBay, InventoryBayLot, ProductLot
 from .forms import ProductLotForm, InventoryTransferForm
@@ -21,7 +20,7 @@ def create_item(request):
         if form.is_valid():
             new_product_lot = form.save()
 
-            starting_bay = get_object_or_404(InventoryBay, name="Operations")
+            starting_bay = get_object_or_404(InventoryBay, name="Production")
 
             InventoryBayLot.objects.create(
                 inventory_bay=starting_bay,
@@ -83,13 +82,11 @@ def transfer(request):
 
 @login_required(login_url=f'/{settings.SITE_PREFIX}accounts/login')
 def find_lot(request):
-    # Get the search query from the URL parameters
     query = request.GET.get('q', '')
     inventory_bay_lots = InventoryBayLot.objects.filter(
         quantity__gt=0).select_related('inventory_bay__warehouse_name', 'product_lot')
 
     if query:
-        # Filter by Product Lot's lot number, Inventory Bay's name, or Warehouse's name
         inventory_bay_lots = inventory_bay_lots.filter(
             Q(product_lot__lot_number__icontains=query) |
             Q(inventory_bay__friendly_name__icontains=query) |
