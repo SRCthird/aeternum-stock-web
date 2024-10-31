562c09c74de763519022772abeeb518f6db79164
Stephen Chryn <M312559@one.merckgroup.com>
2024-10-31 13:51:44 -0400

    Create log entry admin model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,13 +1,54 @@
 from django.contrib import admin
-from simple_history.admin import SimpleHistoryAdmin
-from . import models
-from csvexport.actions import csvexport
-import csv
+from django.contrib.admin.models import LogEntry, DELETION
+from django.urls import reverse
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 from django.http import HttpResponse
 from django.conf import settings
 
+from simple_history.admin import SimpleHistoryAdmin
+from csvexport.actions import csvexport
+import csv
+
+from . import models
+
 admin.AdminSite.site_header = "Aeternum Stock - Admin Center"
 admin.site.site_url = f"../../{settings.SITE_PREFIX}"
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+    list_filter = ['user', 'content_type', 'action_flag']
+    search_fields = ['object_repr', 'change_message']
+    list_display = ['action_time', 'user',
+                    'content_type', 'object_link', 'action_flag', ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = '<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' %
+                        (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return mark_safe(link)
+    object_link.admin_order_field = "object_repr"
+    object_link.short_description = "object"
 
 
 @admin.register(models.Product)
