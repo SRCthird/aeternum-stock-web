101a4cf18bdf6d28814d5ff72105cc69ef1f0a3b
Stephen Chryn <SRCthird@gmail.com>
2024-11-07 06:19:26 -0500

    add searchable dropdown

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('inventory_bays/<int:product_lot_id>/',
          views.get_inventory_bays_for_lot, name='get_inventory_bays_for_lot'),
+    path('product-search/', views.product_search, name='product_search'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
 from django.http import JsonResponse
-from inventory.models import InventoryBayLot
+from inventory.models import InventoryBayLot, Product
+
+
+def product_search(request):
+    query = request.GET.get('q', '')
+    products = Product.objects.filter(name__icontains=query)[:10]
+    results = [{'id': product.id, 'name': product.name} for product in products]
+    return JsonResponse(results, safe=False)
 
 
 def get_inventory_bays_for_lot(request, product_lot_id):
     """Returns Inventory Bays where the given ProductLot has a quantity > 0."""
     inventory_data = []
 
-    # Get Inventory Bays with quantities > 0 for the selected ProductLot
     bays_with_lot = InventoryBayLot.objects.filter(
         product_lot_id=product_lot_id,
         quantity__gt=0
@@ -19,4 +25,3 @@ def get_inventory_bays_for_lot(request, product_lot_id):
         })
 
     return JsonResponse({'bays': inventory_data})
-
