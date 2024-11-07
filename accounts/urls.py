68f6f57863d2dc4195b8d59cd2d21f9bb84f2c76
Stephen Chryn <SRCthird@gmail.com>
2024-11-07 09:38:08 -0500

    Fix logout not sticking

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('logout', views.login_view, name='logout'),
     path('register/', views.register_view, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -26,12 +26,6 @@ def login_view(request):
     return render(request, 'registration/login.html')
 
 
-def logout_view(request):
-    logout(request)
-    request.session.flush()
-    return redirect('login')
-
-
 def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
